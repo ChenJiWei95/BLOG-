96a2a64556aef295b19ef09e7ac4565985c72b4c
chenjiwey <chenjiwey@163.com>
2019-06-06 18:28:44 +0800

    h

--- MyBlog/src/com/blog/Constant.java
+++ MyBlog/src/com/blog/Constant.java
@@ -3,5 +3,5 @@ package com.blog;
 public class Constant {
 	public static String RESCODE_0000 = "0000";//成功
 	public static String RESCODE_0001 = "0001";//失败
-	public static String RESCODE_0002 = "0002";//异常
+	public static String RESCODE_0002 = "0002";//异常    
 }
