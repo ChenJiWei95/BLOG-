4d6079ddf1cc42811f2675754174442d1031a405
chen.jiwei <chenjiwey@163.com>
2019-06-12 23:13:43 +0800

    1
    Signed-off-by: chen.jiwei <chenjiwey@163.com>

--- MyBlog/WebContent/layuiadmin/modules/message.js
+++ MyBlog/WebContent/layuiadmin/modules/message.js
@@ -1,9 +1,9 @@
 ;
 layui.define(["admin", "table", "util"],
 function(e) {
-    var t = layui.$,
-    i = (layui.admin, layui.table),
-    l = (layui.element, {
+    var jquery = layui.$,
+    table = (layui.admin, layui.table),
+    adapter = (layui.element, {//数据暂存 适配 对准type
         all: {
             text: "全部消息",
             id: "LAY-app-message-all"
@@ -20,7 +20,7 @@ function(e) {
     a = function(e) {//这里通过模板调用 e为默认传进来的数据
         return '<a href="detail.html?id=' + e.id + '">' + e.title
     };
-    i.render({//消息通知
+    table.render({//消息通知
         elem: "#LAY-app-message-all",
         url: 'http://localhost:8080/MyBlog/api/test/message/.do',
         page: !0,
@@ -42,7 +42,7 @@ function(e) {
         }]],
         skin: "line"
     }),
-    i.render({
+    table.render({
         elem: "#LAY-app-message-notice",
         url: layui.setter.base + "json/message/notice.js",
         page: !0,
@@ -64,7 +64,7 @@ function(e) {
         }]],
         skin: "line"
     }),
-    i.render({
+    table.render({
         elem: "#LAY-app-message-direct",
         url: layui.setter.base + "json/message/direct.js",
         page: !0,
@@ -86,9 +86,9 @@ function(e) {
         }]],
         skin: "line"
     });
-    var d = {
+    var active = {
         del: function(e, t) {
-            var a = l[t],
+            var a = adapter[t],
             d = i.checkStatus(a.id),
             s = d.data;
             return 0 === s.length ? layer.msg("未选中行") : void layer.confirm("确定删除选中的数据吗？",
@@ -100,7 +100,7 @@ function(e) {
             })
         },
         ready: function(e, t) {
-            var a = l[t],
+            var a = adapter[t],
             d = i.checkStatus(a.id),
             s = d.data;
             return 0 === s.length ? layer.msg("未选中行") : (layer.msg("标记已读成功", {
@@ -108,18 +108,18 @@ function(e) {
             }), void i.reload(a.id))
         },
         readyAll: function(e, t) {
-            var i = l[t];
+            var i = adapter[t];
             layer.msg(i.text + "：全部已读", {
                 icon: 1
             })
         }
     };
-    t(".LAY-app-message-btns .layui-btn").on("click",
+    jquery(".LAY-app-message-btns .layui-btn").on("click",
     function() {
-        var e = t(this),
-        i = e.data("events"),
-        l = e.data("type");
-        d[i] && d[i].call(this, e, l)
+        var e = jquery(this),
+        "events" = e.data("events"),
+        type = e.data("type");
+        active["events"] && active["events"].call(this, e, type)
     }),
     e("message", {})
 });
