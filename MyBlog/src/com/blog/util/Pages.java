fb4b4b54d9562b985120112e2bb2aa7729a9994f
chenjiwey <chenjiwey@163.com>
2019-09-30 18:17:32 +0800

    1

---
+++ MyBlog/src/com/blog/util/Pages.java
@@ -0,0 +1,31 @@
+package com.blog.util;
+
+public class Pages {
+
+	private int page;
+	private int limit;
+	private int offset;
+	
+	
+	
+	
+	public int getOffset() {
+		return offset;
+	}
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public int getLimit() {
+		return limit;
+	}
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
+	
+}
